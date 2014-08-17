287cc20d12f73b9c9bc3df0fce2f4af0aba8ab02
Thomas Watson Steen <w@tson.dk>
2014-08-17 16:53:46 +0200

    Support all native BSON types when not running in the browser

---
+++ browser.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (obj) {
+  var key, val;
+  for (key in obj) {
+    if (!obj.hasOwnProperty(key)) continue;
+    val = obj[key];
+    switch (key) {
+      case '$date':
+        return new Date(val);
+      case '$regex':
+      case '$options':
+        return new RegExp(obj.$regex, obj.$options);
+      case '$undefined':
+        return undefined;
+    }
+    if (typeof val === 'object')
+      obj[key] = module.exports(val);
+  }
+  return obj;
+};
--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 'use strict';
 
-module.exports = function (query) {
+var mongo = require('mongodb');
+
+module.exports = function (obj) {
   var key, val;
-  for (key in query) {
-    if (!query.hasOwnProperty(key)) continue;
-    val = query[key];
+  for (key in obj) {
+    if (!obj.hasOwnProperty(key)) continue;
+    val = obj[key];
     switch (key) {
-    case '$date':
-      return new Date(val);
-    case '$regex':
-      return new RegExp(val, query.$options);
-    case '$undefined':
-      return undefined;
+      case '$binary':
+      case '$type':
+        // TODO: Will this behave if $type isn't set?
+        return new mongo.Binary(obj.$binary, obj.$type);
+      case '$date':
+        return new Date(val);
+      case '$timestamp':
+        return new mongo.Timestamp(val.t, val.i);
+      case '$regex':
+      case '$options':
+        return new RegExp(obj.$regex, obj.$options);
+      case '$oid':
+        return new mongo.ObjectID(val);
+      case '$ref':
+      case '$id':
+        // TODO: Does this follow the mongo.DBRef interface?
+        return new mongo.DBRef(obj.$ref, obj.$id);
+      case '$undefined':
+        return undefined;
+      case '$minKey':
+        return new mongo.MinKey();
+      case '$maxKey':
+        return new mongo.MaxKey();
+      case '$numberLong':
+        return new mongo.Long(val);
     }
     if (typeof val === 'object')
-      query[key] = module.exports(val);
+      obj[key] = module.exports(val);
   }
-  return query;
+  return obj;
 };
